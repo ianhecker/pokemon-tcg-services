961b63f45d598fc6d9f7ad6a64dc43ab7ef5140e
ian hecker <ianhecker.dev@gmail.com>
2025-08-14 13:59:45 -0500

    Chore[request] Add getters

--- client/request.go
+++ client/request.go
@@ -1,7 +1,18 @@
 package client
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+)
 
 type Request struct {
 	req *http.Request
+}
+
+func (r Request) Method() string {
+	return r.req.Method
+}
+
+func (r Request) URL() *url.URL {
+	return r.req.URL
 }
