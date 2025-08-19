9f9001fc502d92fd32d4634adf26744f584991de
ian hecker <ianhecker.dev@gmail.com>
2025-08-19 16:55:24 -0500

    Feature[retryable] Add retryables

--- retry/retry.go
+++ retry/retry.go
@@ -1,8 +1,14 @@
 package retry
 
 import (
+	"context"
+	"errors"
 	"time"
 )
+
+type RetryableInterface interface {
+	Retry(ctx context.Context) (bool, error)
+}
 
 type RetryState int
 
@@ -12,18 +18,72 @@ const (
 	RetryWithBackoff
 )
 
-type RetryFunc func() bool
+type RetryFunc func(ctx context.Context) (RetryState, error)
 
 type Retryable struct {
 	Attempt int
+	Retries int
 	State   RetryState
-	Delay   time.Duration
+	Sleep   time.Duration
 	Do      RetryFunc
+	Body    []byte
 }
 
-func (retry *Retryable) Retry() bool {
-	retry.Attempt++
-	time.Sleep(retry.Delay)
+func MakeRetryable(
+	retries int,
+	sleep time.Duration,
+	do RetryFunc,
+) RetryableInterface {
+	return &Retryable{
+		Retries: retries,
+		Sleep:   sleep,
+		Do:      do,
+		State:   RetryNoBackoff,
+	}
+}
 
-	return true
+func (r *Retryable) Retry(ctx context.Context) (bool, error) {
+	if r.State == NoRetry {
+		return false, errors.New("retry state was not retriable")
+	}
+	if r.Retries <= 0 {
+		return false, errors.New("retries were zero")
+	}
+	if r.Do == nil {
+		return false, errors.New("nil Do function")
+	}
+
+	r.Retries--
+	r.Attempt++
+
+	if r.State == RetryWithBackoff && r.Attempt > 1 && r.Sleep > 0 {
+		timer := time.NewTimer(r.Sleep)
+		defer timer.Stop()
+
+		select {
+		case <-ctx.Done():
+			return false, ctx.Err()
+		case <-timer.C:
+		}
+	}
+
+	newState, err := r.Do(ctx)
+	r.State = newState
+
+	if newState == NoRetry || err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func Do(ctx context.Context, retryable RetryableInterface) error {
+	for {
+		retry, err := retryable.Retry(ctx)
+		if err != nil {
+			return err
+
+		} else if retry != true {
+			return nil
+		}
+	}
 }
--- retry/retry_test.go
+++ retry/retry_test.go
@@ -1,1 +1,51 @@
 package retry_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ianhecker/pokemon-tcg-services/internal/mocks"
+	"github.com/ianhecker/pokemon-tcg-services/retry"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetryable_Retry(t *testing.T) {
+	t.Run("", func(t *testing.T) {})
+	t.Run("", func(t *testing.T) {})
+	t.Run("", func(t *testing.T) {})
+}
+
+func TestDo(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		ctx := context.Background()
+
+		retryable := mocks.NewMockRetryableInterface(t)
+		retryable.On("Retry", ctx).Return(false, nil)
+
+		err := retry.Do(ctx, retryable)
+		assert.NoError(t, err)
+	})
+
+	t.Run("retry", func(t *testing.T) {
+		ctx := context.Background()
+
+		retryable := mocks.NewMockRetryableInterface(t)
+		retryable.On("Retry", ctx).Return(true, nil)
+
+		err := retry.Do(ctx, retryable)
+		assert.NoError(t, err)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		ctx := context.Background()
+
+		expected := errors.New("error")
+
+		retryable := mocks.NewMockRetryableInterface(t)
+		retryable.On("Retry", ctx).Return(false, expected)
+
+		err := retry.Do(ctx, retryable)
+		assert.ErrorIs(t, err, expected)
+	})
+}
