91fb999705269c010b939957fefd49a3dbdc70c1
ian hecker <ianhecker.dev@gmail.com>
2025-08-19 16:59:54 -0500

    Feature[main.go] Cobble together working features

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"time"
 
 	"go.uber.org/zap"
 
 	"github.com/ianhecker/pokemon-tcg-services/networking"
+	"github.com/ianhecker/pokemon-tcg-services/networking/pokemontcgio"
+	v2 "github.com/ianhecker/pokemon-tcg-services/networking/pokemontcgio/v2"
+	"github.com/ianhecker/pokemon-tcg-services/retry"
 )
 
 func main() {
@@ -13,12 +21,32 @@ func main() {
 	defer logger.Sync()
 	sugar := logger.Sugar()
 
-	client := networking.NewClient(sugar, networking.Timeout)
+	httpClient := networking.NewClient(sugar)
 
-	_, _, err := client.Get("https://api.pokemontcg.io/v2/cards/xy1-1")
+	api := v2.CardByID("xy1-1")
+	url := fmt.Sprintf("https://%s", api)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
+	defer cancel()
+
+	perAttemptTimeout := 60 * time.Second
+	client := pokemontcgio.NewClient(httpClient, perAttemptTimeout)
+
+	result, retryFunc := client.MakeRetryFunc(url)
+
+	retries := 10
+	backoff := 2 * time.Second
+	retryable := retry.MakeRetryable(retries, backoff, retryFunc)
+
+	retryErr := retry.Do(ctx, retryable)
+	checkErr(sugar, retryErr)
+
+	var out bytes.Buffer
+	err := json.Indent(&out, result.Body, "", " ")
 	checkErr(sugar, err)
 
-	// fmt.Println(string(body))
+	fmt.Println(out.String())
 }
 
 func checkErr(logger *zap.SugaredLogger, err error) {
