d082b2b225605d4d8da9f247e0c2254cb63514b7
ian hecker <ianhecker.dev@gmail.com>
2025-08-21 10:29:23 -0500

    Chore[pokemontcg.io] Add testing for client

--- pokemontcgio/v2/client_test.go
+++ pokemontcgio/v2/client_test.go
@@ -1,1 +1,193 @@
 package v2_test
+
+import (
+	"context"
+	"errors"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+
+	"github.com/ianhecker/pokemon-tcg-services/internal/mocks"
+	v2 "github.com/ianhecker/pokemon-tcg-services/pokemontcgio/v2"
+	"github.com/ianhecker/pokemon-tcg-services/retry"
+)
+
+func TestClient_MakeRetryFunc(t *testing.T) {
+	t.Run("deadline exceeded", func(t *testing.T) {
+		url := "url"
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(nil, 0, context.DeadlineExceeded)
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1)
+		defer cancel()
+
+		state, err := retryFunc(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, retry.WithBackoff, state)
+		assert.ErrorIs(t, result.Err, context.DeadlineExceeded)
+	})
+
+	t.Run("canceled", func(t *testing.T) {
+		url := "url"
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1)
+
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(nil, 0, context.Canceled).
+			Run(func(args mock.Arguments) {
+				time.Sleep(1 * time.Millisecond)
+				cancel()
+			})
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		state, err := retryFunc(ctx)
+		assert.ErrorIs(t, err, context.Canceled)
+		assert.Equal(t, retry.No, state)
+		assert.ErrorIs(t, result.Err, context.Canceled)
+	})
+
+	t.Run("networking error", func(t *testing.T) {
+		url := "url"
+
+		var netError error = &net.OpError{
+			Op:  "dial",
+			Net: "tcp",
+			Err: &net.DNSError{Err: "i/o timeout", IsTimeout: true},
+		}
+
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(nil, 0, netError)
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1)
+		defer cancel()
+
+		state, err := retryFunc(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, retry.WithBackoff, state)
+		assert.ErrorAs(t, result.Err, &netError)
+	})
+
+	t.Run("status no retry", func(t *testing.T) {
+		body := []byte(`{"message":"hi"}`)
+		status := 200
+		expectedErr := errors.New("error")
+		url := "url"
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(body, status, expectedErr)
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		state, err := retryFunc(context.Background())
+		assert.ErrorIs(t, expectedErr, err)
+		assert.Equal(t, retry.No, state)
+
+		assert.Equal(t, string(body), string(result.Body))
+		assert.Equal(t, status, result.Status)
+		assert.ErrorIs(t, expectedErr, result.Err)
+	})
+
+	t.Run("status retry", func(t *testing.T) {
+		body := []byte(`{"message":"hi"}`)
+		status := 504
+		url := "url"
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(body, status, nil)
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		state, err := retryFunc(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, retry.Yes, state)
+
+		assert.Equal(t, string(body), string(result.Body))
+		assert.Equal(t, status, result.Status)
+		assert.NoError(t, result.Err)
+	})
+
+	t.Run("status retry with backoff", func(t *testing.T) {
+		body := []byte(`{"message":"hi"}`)
+		status := 429
+		url := "url"
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(body, status, nil)
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		state, err := retryFunc(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, retry.WithBackoff, state)
+
+		assert.Equal(t, string(body), string(result.Body))
+		assert.Equal(t, status, result.Status)
+		assert.NoError(t, result.Err)
+	})
+
+	t.Run("status fall through", func(t *testing.T) {
+		body := []byte(`{"message":"hi"}`)
+		status := 0
+		url := "url"
+		mockClient := mocks.NewMockClientInterface(t)
+		mockClient.
+			On("Get", mock.Anything, url).
+			Return(body, status, nil)
+
+		logger := zap.NewNop().Sugar()
+		timeout := 100 * time.Millisecond
+
+		client := v2.NewClient(logger, mockClient, timeout)
+		result, retryFunc := client.MakeRetryFunc(url)
+
+		state, err := retryFunc(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, retry.No, state)
+
+		assert.Equal(t, string(body), string(result.Body))
+		assert.Equal(t, status, result.Status)
+		assert.NoError(t, result.Err)
+	})
+}
