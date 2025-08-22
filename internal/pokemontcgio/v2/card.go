830f5333eaac7ff83bfa31636cc2f5d8040bce97
ian hecker <ianhecker.dev@gmail.com>
2025-08-22 12:34:01 -0500

    Chore[V2] Add card id regex

--- internal/pokemontcgio/v2/card.go
+++ internal/pokemontcgio/v2/card.go
@@ -2,10 +2,40 @@ package v2
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 const CardByIDFmt string = "api.pokemontcg.io/v2/cards/%s"
 
+var CardIDRegex = regexp.MustCompile(`^[A-Za-z0-9._:-]+$`)
+
 func CardByID(ID string) string {
 	return fmt.Sprintf(CardByIDFmt, ID)
+}
+
+func SanitizeCardID(ID string) (string, error) {
+	sani := strings.TrimSpace(ID)
+	if sani == "" || !CardIDRegex.MatchString(sani) {
+		return sani, fmt.Errorf("invalid card ID: %s", sani)
+	}
+	return sani, nil
+}
+
+func SanitizeCardIDs(IDs ...string) ([]string, error) {
+	var sanitized []string
+	var badIDs []string
+
+	for _, ID := range IDs {
+		sani, err := SanitizeCardID(ID)
+		if err != nil {
+			badIDs = append(badIDs, sani)
+		} else {
+			sanitized = append(sanitized, sani)
+		}
+	}
+	if len(badIDs) > 0 {
+		return badIDs, fmt.Errorf("invalid card IDs: %v", badIDs)
+	}
+	return sanitized, nil
 }
--- internal/pokemontcgio/v2/card_test.go
+++ internal/pokemontcgio/v2/card_test.go
@@ -16,3 +16,62 @@ func TestCardByID(t *testing.T) {
 		assert.Equal(t, api, fmt.Sprintf("api.pokemontcg.io/v2/cards/%s", card))
 	})
 }
+
+func TestCardByID_SanitizeCardID(t *testing.T) {
+	var tests = []struct {
+		name      string
+		ID        string
+		sanitized string
+		valid     bool
+	}{
+		{"happy path", "xy1-1", "xy1-1", true},
+		{"happy path trim space", " xy1-1 ", "xy1-1", true},
+		{"empty", "", "", false},
+		{"bad ID", "bad ID", "bad ID", false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sanitized, err := v2.SanitizeCardID(test.ID)
+
+			if test.valid {
+				assert.Nil(t, err)
+			} else {
+				assert.ErrorContains(t, err, "invalid card ID: "+test.ID)
+			}
+			assert.Equal(t, test.sanitized, sanitized)
+		})
+	}
+}
+
+func TestCardByID_AreValidCardIDs(t *testing.T) {
+	var tests = []struct {
+		name     string
+		IDs      []string
+		expected []string
+		valid    bool
+	}{
+		{"happy path", []string{"xy1-1"}, []string{"xy1-1"}, true},
+		{"happy path list", []string{"xy1-1", "xy2-2"}, []string{"xy1-1", "xy2-2"}, true},
+		{"happy path trim space", []string{" xy1-1 "}, []string{"xy1-1"}, true},
+		{"happy path trim space list", []string{" xy1-1 ", " xy2-2 "}, []string{"xy1-1", "xy2-2"}, true},
+		{"empty", []string{""}, []string{""}, false},
+		{"bad ID", []string{"bad ID"}, []string{"bad ID"}, false},
+		{"bad ID list", []string{"bad ID 1", "bad ID 2"}, []string{"bad ID 1", "bad ID 2"}, false},
+		{"bad ID w valid", []string{"base1-1", "bad ID 1", "bad ID 2"}, []string{"bad ID 1", "bad ID 2"}, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			IDs, err := v2.SanitizeCardIDs(test.IDs...)
+
+			if test.valid {
+				assert.Nil(t, err)
+				assert.Equal(t, test.expected, IDs)
+			} else {
+				assert.ErrorContains(t, err, fmt.Sprintf("invalid card IDs: %v", IDs))
+				assert.Equal(t, test.expected, IDs)
+			}
+		})
+	}
+}
