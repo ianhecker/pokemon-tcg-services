ab10869b427e4f1ada1393fd47dd2467786b1b67
ian hecker <ianhecker.dev@gmail.com>
2025-08-25 16:09:20 -0500

    Refactor[CardID] Make Card ID

--- internal/pokemontcg/card.go
+++ internal/pokemontcg/card.go
@@ -8,27 +8,24 @@ import (
 
 var CardIDRegex = regexp.MustCompile(`^[A-Za-z0-9._:-]+$`)
 
+type CardID string
+
+func (cardID CardID) String() string {
+	return string(cardID)
+}
+
 type Card struct {
-	ID     string
-	Name   string `json:"name"`
-	Number int    `json:"number"`
-	Set    Set    `json:"set"`
+	ID          CardID
+	Name        string `json:"name"`
+	Number      int    `json:"number,string"`
+	Set         Set    `json:"set"`
+	LastUpdated string `json:"lastUpdated"`
 }
 
-func MakeCard(ID string) (Card, error) {
-	cardID, err := SanitizeID(ID)
-	if err != nil {
-		return Card{}, err
-	}
-	return Card{
-		ID: cardID,
-	}, nil
-}
-
-func SanitizeID(ID string) (string, error) {
+func MakeCardID(ID string) (CardID, error) {
 	sani := strings.TrimSpace(ID)
 	if sani == "" || !CardIDRegex.MatchString(sani) {
 		return "", fmt.Errorf("invalid card ID: %s", sani)
 	}
-	return sani, nil
+	return CardID(sani), nil
 }
--- internal/pokemontcg/card_test.go
+++ internal/pokemontcg/card_test.go
@@ -8,20 +8,7 @@ import (
 	"github.com/ianhecker/pokemon-tcg-services/internal/pokemontcg"
 )
 
-func TestCard_MakeCard(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
-		card, err := pokemontcg.MakeCard("base1-1")
-		assert.NoError(t, err)
-		assert.Equal(t, "base1-1", card.ID)
-	})
-
-	t.Run("bad ID", func(t *testing.T) {
-		_, err := pokemontcg.MakeCard("bad ID")
-		assert.ErrorContains(t, err, "invalid card ID: bad ID")
-	})
-}
-
-func TestCardByID_SanitizeCardID(t *testing.T) {
+func TestSanitizeCardID(t *testing.T) {
 	var tests = []struct {
 		name      string
 		ID        string
@@ -36,11 +23,11 @@ func TestCardByID_SanitizeCardID(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			sanitized, err := pokemontcg.SanitizeID(test.ID)
+			ID, err := pokemontcg.MakeCardID(test.ID)
 
 			if test.valid {
 				assert.Nil(t, err)
-				assert.Equal(t, test.sanitized, sanitized)
+				assert.Equal(t, test.sanitized, ID.String())
 			} else {
 				assert.ErrorContains(t, err, "invalid card ID: "+test.ID)
 			}
