7d0e9748e77b64da12f2bc63de312630d486aecc
ian hecker <ianhecker.dev@gmail.com>
2025-08-26 13:53:08 -0500

    Refactor[cardpricer] Rename package

--- internal/services/cardpricer/handler.go
+++ internal/services/cardpricer/handler.go
@@ -1,4 +1,4 @@
-package card
+package cardpricer
 
 import (
 	"context"
--- internal/services/cardpricer/parse.go
+++ internal/services/cardpricer/parse.go
@@ -1,4 +1,4 @@
-package card
+package cardpricer
 
 import (
 	"errors"
@@ -12,16 +12,14 @@ func ParseCardQuery(r *http.Request) (pokemontcg.CardID, error) {
 	if r == nil {
 		return "", errors.New("request is nil")
 	}
-
 	q := r.URL.Query()
 	queryStrings := q["id"]
 	if len(queryStrings) == 0 {
-		return "", fmt.Errorf("missing required query parameter: id")
+		return "", errors.New("missing required query: id")
 	}
-
 	ID, err := pokemontcg.MakeCardID(queryStrings[0])
 	if err != nil {
-		return "", fmt.Errorf("error sanitizing card ID: %w", err)
+		return "", fmt.Errorf("error with query: %w", err)
 	}
 	return ID, nil
 }
