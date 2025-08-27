35a20e37125a201995ee7aa130bb3fa946af21f7
ian hecker <ianhecker.dev@gmail.com>
2025-08-27 16:27:56 -0500

    Chore[set] Delete type set

--- internal/pokemontcg/set.go
+++
@@ -1,9 +0,0 @@
-package pokemontcg
-
-type Set struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Series      string `json:"series"`
-	Total       int    `json:"total"`
-	ReleaseDate string `json:"releaseDate"`
-}
