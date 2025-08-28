35adb56d14640aca32d7674c14243d4af6f3a587
ian hecker <ianhecker.dev@gmail.com>
2025-08-28 16:48:07 -0500

    Feature[config] Read in env vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,22 +1,24 @@
 package config
 
-import "strings"
+import (
+	"log"
+	"strings"
 
-type Config struct {
-}
+	"github.com/spf13/viper"
+)
 
 type Token struct {
 	token string
 }
 
-func NewToken(token string) *Token {
-	return &Token{
+func MakeToken(token string) Token {
+	return Token{
 		token: token,
 	}
 }
 
-func (t Token) Inject() string {
-	return string(t.token)
+func (t Token) Reveal() string {
+	return t.token
 }
 
 func (t Token) String() string {
@@ -25,13 +27,31 @@ func (t Token) String() string {
 
 func (t Token) obfuscate() string {
 	length := len(t.token)
-
 	if length >= 10 {
-		start := t.token[:3]
-		middle := strings.Repeat("*", length-6)
-		end := t.token[length-3:]
-
-		return start + middle + end
+		return t.token[:3] + strings.Repeat("*", length-6) + t.token[length-3:]
 	}
 	return strings.Repeat("*", length)
+}
+
+type Config struct {
+	JustTCG struct {
+		APIKey Token
+	}
+}
+
+func (config Config) FlightCheck() {
+	if config.JustTCG.APIKey.Reveal() == "" {
+		log.Fatal("config flight check: Just TCG API Key is empty")
+	}
+}
+
+func Load() (Config, error) {
+	_ = viper.BindEnv("JUST_TCG_API_KEY")
+	viper.AutomaticEnv()
+
+	var config Config
+	token := MakeToken(viper.GetString("JUST_TCG_API_KEY"))
+	config.JustTCG.APIKey = token
+
+	return config, nil
 }
