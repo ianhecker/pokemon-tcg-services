efdeda709567308f9f813e00491db6e326738c58
ian hecker <ianhecker.dev@gmail.com>
2025-09-23 11:55:18 -0500

    Refactor[cards] Remove logic from variant

--- internal/justtcg/v1/cards/variant.go
+++ internal/justtcg/v1/cards/variant.go
@@ -1,32 +1,14 @@
 package cards
 
 import (
-	"fmt"
-
 	"github.com/ianhecker/pokemon-tcg-services/internal/pokemontcg"
 )
 
 type Variant struct {
+	Printing    Printing             `json:"printing"`
 	Condition   pokemontcg.Condition `json:"condition"`
 	Price       float64              `json:"price"`
 	MinPrice30d float64              `json:"minPrice30d"`
 	MaxPrice30d float64              `json:"maxPrice30d"`
 	LastUpdated int64                `json:"lastUpdated"`
-}
-
-type Variants []Variant
-
-func (variants Variants) ToMap() (map[pokemontcg.Condition]Prices, error) {
-
-	m := make(map[pokemontcg.Condition]Prices)
-	for _, v := range variants {
-		condition := v.Condition
-
-		_, exists := m[condition]
-		if exists {
-			return nil, fmt.Errorf("duplicated condition in prices: '%s'", condition)
-		}
-		m[condition] = MakePricesFromVariant(v)
-	}
-	return m, nil
 }
