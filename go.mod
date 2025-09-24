ecafd2993db529fdaeb18014e9c43e0b18bc1015
ian hecker <ianhecker.dev@gmail.com>
2025-09-24 15:43:12 -0500

    Refactor[cards] Overhaul of testing. Use internal structs over external testdata

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/stretchr/testify v1.10.0
 require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
--- internal/justtcg/v1/cards/map_test.go
+++ internal/justtcg/v1/cards/map_test.go
@@ -4,7 +4,9 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -28,39 +30,126 @@ func TestValidate(t *testing.T) {
 		err := cards.Validate(responseDTO)
 		assert.NoError(t, err)
 	})
+	t.Run("errors", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			response cards.ResponseDTO
+			err      string
+		}{
+			{"empty data", cards.ResponseDTO{Data: nil}, "data field is empty"},
+			{"empty TCGPlayerID", cards.ResponseDTO{Data: []cards.CardDTO{{TCGPlayerID: ""}}}, "TCGPlayerId is empty"},
+			{"empty variants", cards.ResponseDTO{Data: []cards.CardDTO{{TCGPlayerID: "106999", Variants: nil}}}, "variants are empty"},
+			{"empty printing", cards.ResponseDTO{Data: []cards.CardDTO{{TCGPlayerID: "106999", Variants: []cards.VariantDTO{{Printing: ""}}}}}, "printing is empty"},
+			{"empty condition", cards.ResponseDTO{Data: []cards.CardDTO{{TCGPlayerID: "106999", Variants: []cards.VariantDTO{{Printing: "1st Edition Holofoil", Condition: ""}}}}}, "condition is empty"},
+		}
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
 
-	t.Run("empty data", func(t *testing.T) {
-		responseDTO := readTestData(t, "/validate/empty/data.json")
+				err := cards.Validate(test.response)
+				assert.ErrorContains(t, err, test.err)
+			})
+		}
+	})
+}
 
-		err := cards.Validate(responseDTO)
-		assert.ErrorContains(t, err, "data field is empty")
+func TestMap(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		responseDTO := readTestData(t, "response.json")
+
+		got, err := cards.Map(responseDTO)
+		require.NoError(t, err)
+
+		equal := cmp.Equal(expected, got)
+		assert.True(t, equal, cmp.Diff(expected, got))
 	})
 
-	t.Run("empty TCGPlayer ID", func(t *testing.T) {
-		responseDTO := readTestData(t, "/validate/empty/tcgplayerid.json")
+	t.Run("errors", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			filepath string
+			err      string
+		}{
+			{"validation error", "bad/data.json", "error validating response"},
+			{"TCGPlayerID error", "bad/tcgplayerid.json", "error parsing TCGPlayerID"},
+			{"variant error", "bad/variants.json", "error mapping variants"},
+		}
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
 
-		err := cards.Validate(responseDTO)
-		assert.ErrorContains(t, err, "card: TCGPlayerId is empty")
+				responseDTO := readTestData(t, test.filepath)
+				_, err := cards.Map(responseDTO)
+				assert.ErrorContains(t, err, test.err)
+			})
+		}
 	})
+}
 
-	t.Run("empty variants", func(t *testing.T) {
-		responseDTO := readTestData(t, "/validate/empty/variants.json")
+func fptr(f float64) *cards.Price {
+	price := cards.Price(f)
+	return &price
+}
 
-		err := cards.Validate(responseDTO)
-		assert.ErrorContains(t, err, "card: variants are empty")
-	})
+func tptr(t time.Time) *time.Time {
+	return &t
+}
 
-	t.Run("empty printing", func(t *testing.T) {
-		responseDTO := readTestData(t, "/validate/empty/printing.json")
-
-		err := cards.Validate(responseDTO)
-		assert.ErrorContains(t, err, "card: variants: printing is empty")
-	})
-
-	t.Run("empty condition", func(t *testing.T) {
-		responseDTO := readTestData(t, "/validate/empty/condition.json")
-
-		err := cards.Validate(responseDTO)
-		assert.ErrorContains(t, err, "card: variants: condition is empty")
-	})
+var expected = cards.Card{
+	ID:          "pokemon-base-set-shadowless-charizard-holo-rare",
+	TCGPlayerID: "106999",
+	Name:        "Charizard",
+	Number:      "004/102",
+	Rarity:      "Holo Rare",
+	Set:         "Base Set (Shadowless)",
+	Prices: cards.Printings{
+		"1st Edition Holofoil": cards.Conditions{
+			NearMint: &cards.Prices{
+				Market:      fptr(503.49),
+				MinPrice30d: fptr(500.99),
+				MaxPrice30d: fptr(503.49),
+				LastUpdated: tptr(time.Unix(1758706033, 0).UTC()),
+			},
+			ModeratelyPlayed: &cards.Prices{
+				Market:      fptr(5495),
+				MinPrice30d: fptr(5495),
+				MaxPrice30d: fptr(5495),
+				LastUpdated: tptr(time.Unix(1754787579, 0).UTC()),
+			},
+			HeavilyPlayed: &cards.Prices{
+				Market:      cards.MakePrice(225),
+				LastUpdated: tptr(time.Unix(1743656485, 0).UTC()),
+			},
+		},
+		"Unlimited Holofoil": cards.Conditions{
+			NearMint: &cards.Prices{
+				Market:      fptr(1923.19),
+				MinPrice30d: fptr(1700),
+				MaxPrice30d: fptr(1923.19),
+				LastUpdated: tptr(time.Unix(1758706033, 0).UTC()),
+			},
+			LightlyPlayed: &cards.Prices{
+				Market:      fptr(1299),
+				MinPrice30d: fptr(1299),
+				MaxPrice30d: fptr(1299),
+				LastUpdated: tptr(time.Unix(1758706033, 0).UTC()),
+			},
+			ModeratelyPlayed: &cards.Prices{
+				Market:      fptr(764.78),
+				MinPrice30d: fptr(761.02),
+				MaxPrice30d: fptr(813.73),
+				LastUpdated: tptr(time.Unix(1758706033, 0).UTC()),
+			},
+			HeavilyPlayed: &cards.Prices{
+				Market:      fptr(645.05),
+				MinPrice30d: fptr(642.95),
+				MaxPrice30d: fptr(649),
+				LastUpdated: tptr(time.Unix(1758706033, 0).UTC()),
+			},
+			Damaged: &cards.Prices{
+				Market:      fptr(524.96),
+				MinPrice30d: fptr(523.77),
+				MaxPrice30d: fptr(533.29),
+				LastUpdated: tptr(time.Unix(1758706033, 0).UTC()),
+			},
+		},
+	},
 }
